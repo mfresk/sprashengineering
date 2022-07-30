b1da2cae2a7300e1f787ada96b260d7628a531b5
Tushar Ranjan Jena <79927383+mfresk@users.noreply.github.com>
2022-07-30 20:04:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+  navLinks.style.right = "0";
+}
+
+function hideMenu(){
+  navLinks.style.right = "-200px";
+}
+
+var navbar = document.getElementById("navbar");
+var menu = document.getElementById("menu");
+
+window.onscroll = function(){
+  if(window.pageYOffset >= menu.offsetTop){
+    navbar.classList.add("sticky");
+  }
+  else{
+    navbar.classList.remove("sticky");
+  }
+}
